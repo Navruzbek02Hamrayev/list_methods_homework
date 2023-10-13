edd1680a97c7534513ac07ee6e083a1170f397cf
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-13 03:04:53 -0400

    Return the [num1,num2]

--- List10.py
+++ List10.py
@@ -6,4 +6,15 @@ def main(list1):
     Returns:
         list: return answer
     """
-    return
+    i=0
+    num1=0
+    num2=0
+    while i<len(list1):
+        if list1[i]==0:
+            num1+=list1[i]+1
+        elif list1[i]==1:
+            num2+=list1[i]
+        i+=1
+    return [num1,num2]
+print(main([1, 0, 0, 0, 1, 0, 1, 0]))
+print(main([0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1]))
