5f8d235137834cf2899eecd753c858dbd48645ec
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-13 02:39:06 -0400

    Return the list1,sum

--- List09.py
+++ List09.py
@@ -6,4 +6,15 @@ def main(fruits):
     Returns:
         list: return answer
     """
-    return
+    l=[]
+    list1=[]
+    i=0
+    sum=0
+    while i<len(fruits):
+        if fruits[i]=="apple":
+            sum+=1
+            list1.append(i)
+        i+=1
+    return list1,sum
+print(main( ["apple", "banana", "apple", "pear", "apple"]))
+print(main(["apple", "apple", "apple", "apple", "kiwi"]))
